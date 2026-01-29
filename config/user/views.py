badb34490a2749f88186e1d9aceb6b30820b31e0
onix-001 <somakisa79@gmail.com>
2026-01-29 12:44:24 +0100

    push09

--- config/user/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def login(request):
-    return render(request, 'user/login.html')
-  
-def register(request):
-    return render(request, 'user/register.html')
